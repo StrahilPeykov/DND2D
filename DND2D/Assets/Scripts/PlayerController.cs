2a75370164c03d630d42b83a724bf3fa715aedc1
Strahil Peykov <47077761+StrahilPeykov@users.noreply.github.com>
2020-07-09 18:49:43 +0300

    SceneManager

--- DND2D/Assets/Scripts/PlayerController.cs
+++ DND2D/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,22 @@ public class PlayerController : MonoBehaviour
 
     public Animator myAnimator;
 
+    public static PlayerController instance;
+
+    public string areaTransitionName;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        if (instance==null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
