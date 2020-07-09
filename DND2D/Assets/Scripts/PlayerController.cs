128e40e17ea1e231afd63a537d6bbbac1b406957
Strahil Peykov <47077761+StrahilPeykov@users.noreply.github.com>
2020-07-09 13:04:05 +0300

    Player

---
+++ DND2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRigidbody;
+    public float moveSpeed;
+
+    public Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        myAnimator.SetFloat("moveX", theRigidbody.velocity.x);
+        myAnimator.SetFloat("moveY", theRigidbody.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
